06a4682cc3b796b932b27c83057f850b91a85e13
Jason Spafford <nullprogrammer@gmail.com>
2017-02-18 05:59:21 -0800

    Moved last print to logger

--- serverthrall/plugins/downrecovery.py
+++ serverthrall/plugins/downrecovery.py
@@ -5,7 +5,6 @@ class DownRecovery(ServerThrallPlugin):
 
     def tick(self):
         if not self.server.is_running():
-            print('Server currently down. Rebooting')
-            # self.logger.info('Server down... rebooting')
+            self.logger.error('Server down, bringing it back online')
             self.server.close()
             self.server.start()
