7dd279ee1845bdc6f5755f9ddee52fb79daa222a
Jason Spafford <nullprogrammer@gmail.com>
2017-02-21 20:51:17 -0800

    Changed default parameter to default

--- serverthrall/appconfig/thrallconfig.py
+++ serverthrall/appconfig/thrallconfig.py
@@ -8,7 +8,7 @@ class ThrallConfig(SectionForcedConfig):
     def __init__(self, config):
         super(ThrallConfig, self).__init__('ServerThrall', config)
         self.set_default('conan_server_directory', os.path.join(os.getcwd(), 'vendor\\server'))
-        self.set_default('force_update_on_launch', 'true')
+        self.set_default('force_update_on_launch', 'false')
 
     def save(self):
     	config_save(self.config)
