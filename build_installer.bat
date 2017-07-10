e7079e3ee5a74ecbc2e576a3e4912d28116e4af3
Jason Spafford <nullprogrammer@gmail.com>
2017-07-09 23:18:31 -0700

    Add pyinstaller and setup files

---
+++ build_installer.bat
@@ -0,0 +1,11 @@
+pyinstaller main.py^
+ --name serverthrall^
+ --workpath .\installer\build^
+ --distpath .\installer\dist^
+ --specpath .\installer^
+ --icon .\installer\icon.ico^
+ --console^
+ --onedir^
+ --onefile^
+ --noconfirm^
+ --clean^
