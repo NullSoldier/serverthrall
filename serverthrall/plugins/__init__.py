5221783286beb6459646ce218b8dbcedab9f831a
Jason Spafford <nullprogrammer@gmail.com>
2017-07-12 23:46:14 -0700

    Delete raid plugin

--- serverthrall/plugins/__init__.py
+++ serverthrall/plugins/__init__.py
@@ -1,7 +1,6 @@
 #flake8: noqa
 from .apiuploader import ApiUploader
 from .downrecovery import DownRecovery
-from .raidmanager import RaidManager
 from .serverconfig import ServerConfig
 from .serverupdater import ServerUpdater
 from .uptimetracker import UptimeTracker
