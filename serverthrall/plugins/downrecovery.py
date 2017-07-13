ae9adbf615298880226909ec49ac92fa63f7c58f
Jason Spafford <nullprogrammer@gmail.com>
2017-07-13 03:09:41 -0700

    Reduced checking if server down to 30 seconds

--- serverthrall/plugins/downrecovery.py
+++ serverthrall/plugins/downrecovery.py
@@ -1,9 +1,15 @@
-from .thrallplugin import ThrallPlugin
+from .intervaltickplugin import IntervalTickPlugin
 
+class DownRecovery(IntervalTickPlugin):
 
-class DownRecovery(ThrallPlugin):
+    THIRTY_SECONDS = 30
 
-    def tick(self):
+    def __init__(self, config):
+        super(DownRecovery, self).__init__(config)
+        config.set_default('interval.interval_seconds', self.THIRTY_SECONDS)
+        config.queue_save()
+
+    def tick_interval(self):
         if not self.server.is_running():
             self.logger.error('Server down, bringing it back online')
             self.server.close()
