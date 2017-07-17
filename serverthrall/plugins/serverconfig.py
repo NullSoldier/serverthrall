58f38651de8b54e9a0112c666abf4c56678e4400
Jason Spafford <nullprogrammer@gmail.com>
2017-07-16 18:06:12 -0700

    Use filewatcher to watch and update conan configs

--- serverthrall/plugins/serverconfig.py
+++ serverthrall/plugins/serverconfig.py
@@ -1,8 +1,40 @@
-from .thrallplugin import ThrallPlugin
+from .intervaltickplugin import IntervalTickPlugin
 from configparser import NoOptionError
+from contextlib import contextmanager
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import os
 
 
-class ServerConfig(ThrallPlugin):
+class OnModifiedHandler(FileSystemEventHandler):
+
+    def __init__(self, plugin, paths, logger, *args, **kwargs):
+        super(OnModifiedHandler, self).__init__(*args, **kwargs)
+        self.plugin = plugin
+        self.paths = paths
+        self.logger = logger
+        self.ignore = False
+
+    def on_modified(self, ev):
+        if ev.src_path not in self.paths:
+            return
+
+        if self.ignore is True:
+            return
+
+        self.logger.info('got modification %s: %s' % (ev.event_type, ev.src_path))
+        self.plugin.tick_early()
+
+    @contextmanager
+    def ignore_modifications(self):
+        self.ignore = True
+        try:
+            yield
+        finally:
+            self.ignore = True
+
+
+class ServerConfig(IntervalTickPlugin):
 
     CONFIG_MAPPING = {
         'ServerName':           ('Engine', 'OnlineSubsystemSteam', 'ServerName'),
@@ -19,21 +51,57 @@ class ServerConfig(ThrallPlugin):
         'NetServerMaxTickRate': ('Engine', '/Script/OnlineSubsystemUtils.IpNetDriver', 'NetServerMaxTickRate'),
     }
 
-    DEFAULT_WHITE_LIST = []
+    DEFAULT_WHITE_LIST = ['Port']
+    FIVE_MINUTES = 5 * 60
 
-    def config_get_safe(self, src):
+    def __init__(self, config):
+        super(ServerConfig, self).__init__(config)
+        config.set_default('interval.interval_seconds', self.FIVE_MINUTES)
+        config.queue_save()
+
+    def ready(self, *args, **kwargs):
+        super(ServerConfig, self).ready(*args, **kwargs)
+
+        config_paths = self.get_conan_config_paths(self.thrall.conan_config)
+        self.handler = OnModifiedHandler(self, config_paths, self.logger)
+
+        default_config_dir = os.path.join(self.thrall.config.get('conan_server_directory'), 'ConanSandbox\\Config')
+        derived_config_dir = os.path.join(self.thrall.config.get('conan_server_directory'), 'ConanSandbox\\Saved\\Config\\WindowsServer')
+
+        self.observer = Observer()
+        self.observer.schedule(self.handler, path=default_config_dir, recursive=False)
+        self.observer.schedule(self.handler, path=derived_config_dir, recursive=False)
+        self.observer.start()
+
+        self.tick_early()
+
+    def unload(self, *args, **kwargs):
+        if self.observer is not None:
+            self.observer.stop()
+            self.observer.join()
+
+    def get_conan_config_paths(self, conan_config):
+        config_paths = []
+
+        for group_name, group_paths in self.thrall.conan_config.group_paths.items():
+            for group_path in group_paths:
+                config_paths.append(group_path)
+
+        return config_paths
+
+    def get_config_value_safe(self, src):
         try:
             return self.config.get(src)
         except NoOptionError:
             return None
 
-    def sync_mapping(self, mapping):
+    def sync(self):
         changed = False
 
         for src, dest in self.CONFIG_MAPPING.items():
             group, section, option = dest
 
-            value = self.config_get_safe(src)
+            value = self.get_config_value_safe(src)
             original = self.thrall.conan_config.get(group, section, option)
 
             if value is not None and value != original:
@@ -44,12 +112,13 @@ class ServerConfig(ThrallPlugin):
 
         return changed
 
-    def tick(self):
+    def tick_interval(self):
         self.thrall.conan_config.refresh()
-        changed = self.sync_mapping(self.CONFIG_MAPPING)
+        changed = self.sync()
 
         if changed:
             self.logger.info('Restarting server for config to take into affect')
             self.server.close()
-            self.thrall.conan_config.save()
+            with self.handler.ignore_modifications():
+                self.thrall.conan_config.save()
             self.server.start()
