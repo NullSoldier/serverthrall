9602067f24a00a155c66cc614127db45c0f3b6fe
Jason Spafford <nullprogrammer@gmail.com>
2018-05-25 04:22:11 -0700

    Add warning message when RCON is not configured

--- serverthrall/plugins/remoteconsole.py
+++ serverthrall/plugins/remoteconsole.py
@@ -15,6 +15,34 @@ class RemoteConsole(IntervalTickPlugin):
         config.set_default('interval.interval_seconds', self.ONE_MINUTE)
         config.queue_save()
 
+    def ready(self, steamcmd, server, thrall):
+        super(RemoteConsole, self).ready(steamcmd, server, thrall)
+
+        rcon_enabled, _, _, _ = self.get_rcon_auth()
+
+        if not rcon_enabled:
+            self.logger.error(
+                """You have RCON enabled but have not configured RCON in the ServerConfig plugin."""
+                """ See the documentation for more information on the RemoteConsole plugin."""
+                """ https://conanexiles.gamepedia.com/Rcon""")
+
+    def get_rcon_auth(self):
+        rcon_host     = self.server.multihome
+        rcon_port     = self.thrall.conan_config.get_setting(CONAN_SETTINGS_MAPPING['RconPort'])
+        rcon_password = self.thrall.conan_config.get_setting(CONAN_SETTINGS_MAPPING['RconPassword'])
+        rcon_enabled  = self.thrall.conan_config.get_setting(CONAN_SETTINGS_MAPPING['RconEnabled'])
+
+        if not rcon_enabled:
+            rcon_enabled = '0'
+
+        if not rcon_host or rcon_host in ('0.0.0.0'):
+            rcon_host = '127.0.0.1'
+
+        if rcon_enabled != '1' or not rcon_host or not rcon_port or not rcon_password:
+            return False, None, None, None
+
+        return True, rcon_host, rcon_port, rcon_password
+
     def execute_safe(self, command):
         if not self.enabled:
             return None
@@ -22,14 +50,9 @@ class RemoteConsole(IntervalTickPlugin):
         if self.server.running_time() <= timedelta(minutes=1):
             return None
 
-        rcon_host     = self.server.multihome
-        rcon_port     = self.thrall.conan_config.get_setting(CONAN_SETTINGS_MAPPING['RconPort'])
-        rcon_password = self.thrall.conan_config.get_setting(CONAN_SETTINGS_MAPPING['RconPassword'])
+        rcon_enabled, rcon_host, rcon_port, rcon_password = self.get_rcon_auth()
 
-        if not rcon_host or rcon_host in ('0.0.0.0'):
-            rcon_host = '127.0.0.1'
-
-        if not rcon_host or not rcon_port or not rcon_password:
+        if not rcon_enabled:
             return None
 
         command = command.encode('ascii', 'replace')
