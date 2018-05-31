523abf47dd54dd71345b7aa28e26ba17cbbe4435
Jason Spafford <nullprogrammer@gmail.com>
2018-05-31 01:34:08 -0700

    Change paths used in buildinstaller.sh

--- buildinstaller.sh
+++ buildinstaller.sh
@@ -1,19 +1,21 @@
 set -e
 
-rm -rf installer/dist
+rm -rf bin/serverthrall/dist
+mkdir -p bin/serverthrall
+cp ./icon.ico bin/serverthrall/icon.ico
 
 pyinstaller main.py \
 	--name serverthrall \
-	--workpath .\\installer\\build \
-	--distpath .\\installer\\dist \
-	--specpath .\\installer \
-	--icon .\\installer\\icon.ico \
+	--workpath .\\bin\\serverthrall\\build \
+	--distpath .\\bin\\serverthrall\\dist \
+	--specpath .\\bin\\serverthrall \
+	--icon .\\bin\\serverthrall\\icon.ico \
 	--console \
 	--onedir \
 	--onefile \
 	--noconfirm \
 	--clean
 
- mkdir installer/dist/vendor
- mkdir installer/dist/vendor/steamcmd
- cp -r vendor/steamcmd/steamcmd.exe installer/dist/vendor/steamcmd/steamcmd.exe
+ mkdir bin/serverthrall/dist/vendor
+ mkdir bin/serverthrall/dist/vendor/steamcmd
+ cp -r vendor/steamcmd/steamcmd.exe bin/serverthrall/dist/vendor/steamcmd/steamcmd.exe
