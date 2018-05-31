ed8eb417ebc1d13ea4bc64ee5e86334f6cb0648a
Jason Spafford <nullprogrammer@gmail.com>
2018-05-31 01:34:27 -0700

    Fix crash with no language set

--- serverthrall/localization.py
+++ serverthrall/localization.py
@@ -17,12 +17,12 @@ class Localization():
         self.config = config
 
     def return_word(self, key):
-        language = self.config.get('language')
+        language = self.config.get('language', default='en')
 
-        if language is None or language not in language_map:
+        if language not in language_map:
             language = 'en'
 
         if key not in language_map[language]:
             return key
-        
+
         return language_map[language][key]
